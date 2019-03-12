6ecf61fb54ee17f418df7df4b462b66a58726177
nickota <xxk0ta0605@gmail.com>
2019-03-12 22:16:32 +0900

    first commit

---
+++ src/main/java/io/youtubeapp/YoutubeappApplication.java
@@ -0,0 +1,13 @@
+package io.youtubeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeappApplication.class, args);
+	}
+
+}
