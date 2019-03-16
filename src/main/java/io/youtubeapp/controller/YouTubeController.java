56f93fdb0fff46722e4aab9b89e1cbea508c048b
nickota <xxk0ta0605@gmail.com>
2019-03-16 11:32:17 +0900

    link controller to view

--- src/main/java/io/youtubeapp/controller/YouTubeController.java
+++ src/main/java/io/youtubeapp/controller/YouTubeController.java
@@ -1,8 +1,15 @@
 package io.youtubeapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class YouTubeController {
 
+	@RequestMapping(value="/youtube",method=RequestMethod.GET)
+	public String youTube(Model model) {
+		return "youtube";
+	}
 }
