b45b09f6cf31c06729ad358ada48b9ccf8225f60
nickota <xxk0ta0605@gmail.com>
2019-03-16 12:11:32 +0900

    link POST to searchResults.html

--- src/main/java/io/youtubeapp/controller/YouTubeController.java
+++ src/main/java/io/youtubeapp/controller/YouTubeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class YouTubeController {
@@ -11,5 +12,11 @@ public class YouTubeController {
 	@RequestMapping(value="/youtube",method=RequestMethod.GET)
 	public String youTube(Model model) {
 		return "youtube";
+	}
+	
+	@RequestMapping(value="youtube",method=RequestMethod.POST)
+	public String searchMovies(@RequestParam("keyword")String keyword, Model model) {
+		model.addAttribute("keyword",keyword);
+		return "searchResults";
 	}
 }
