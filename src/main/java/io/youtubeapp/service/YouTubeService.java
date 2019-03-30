63edde2dc277bc32d0e5eb4c1e68960509cb27bf
nickota <xxk0ta0605@gmail.com>
2019-03-30 11:37:00 +0900

    Template of YouTubeService.java. Makes list of videos from the queryterm.

--- src/main/java/io/youtubeapp/service/YouTubeService.java
+++ src/main/java/io/youtubeapp/service/YouTubeService.java
@@ -2,18 +2,20 @@ package io.youtubeapp.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
+import io.youtubeapp.model.YouTubeVideo;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
 
 public class YouTubeService {
-	/////////READY TO USE API///////////
+	
+	private static final String API_KEY = "AIzaSyCPxnc8_VVMyps0dqiks6IBZw6dbjt7ENU";
 	private static final long NUMBER_OF_VIDEOS_RETURNED = 5;
 	private static YouTube youtube;
 	//refs https://developers.google.com/youtube/v3/getting-started#fields
@@ -21,28 +23,46 @@ public class YouTubeService {
 
 	public List<YouTubeVideo> searchVideos(String queryTerm){
 		List<YouTubeVideo> videos = new ArrayList<>();
-
-        youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
-            public void initialize(HttpRequest request) throws IOException {
-            }
-        }).setApplicationName("youtube-cmdline-search-sample").build();		
-        
-        /////////////TERMS TO SEARCH//////////////////
-        
-		YouTube.Search.List search = youtube.search().list("id,snippet");
-
-		//set terms to search
-		public static final String API_KEY = "aaa";
-		search.setKey(API_KEY);
-		search.setQ(queryTerm);
-		search.setType("video");
-		search.setFields(YOUTUBE_SEARCH_FIELDS);
-		search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-
-		SearchListResponse searchResponse = search.execute();
 		
-		
-		
-		//////GATHER THE RESULTS/////////
+		try {
+			youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+				public void initialize(HttpRequest request) throws IOException {
+				}
+			}).setApplicationName("youtube-cmdline-search-sample").build();		
+
+			YouTube.Search.List search = youtube.search().list("id,snippet");
+
+			//set terms to connect API
+			search.setKey(API_KEY);
+			search.setQ(queryTerm);
+			search.setType("video");
+			search.setFields(YOUTUBE_SEARCH_FIELDS);
+			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+			
+			//execute search
+			//refs Search.java
+			SearchListResponse searchResponse = search.execute();
+			List<SearchResult> searchResultList = searchResponse.getItems();
+
+			for (SearchResult searchResult : searchResultList) {
+				YouTubeVideo video = new YouTubeVideo();
+				video.setTitle(searchResult.getSnippet().getTitle());
+				video.setUrl(createUrl(searchResult.getId().getVideoId()));
+				video.setThumbnailUrl(searchResult.getSnippet().getThumbnails().getDefault().getUrl());
+				video.setPublishDate(searchResult.getSnippet().getPublishedAt().toString());
+				video.setDescription(searchResult.getSnippet().getDescription());
+				videos.add(video);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return videos;
 	}
+	
+	public String createUrl(String videoId) {
+		//Below is the relation between videoid and url
+		return "https://www.youtube.com/watch?v=" + videoId;
+	}
+	
 }
