25276d19e8148fed51e164f110f3456852b6340e
nickota <xxk0ta0605@gmail.com>
2019-04-16 22:37:22 +0900

    [YouTubeVideoTest] add jUnit

---
+++ src/test/java/io/youtubeapp/YouTubeVideoTest.java
@@ -0,0 +1,14 @@
+package io.youtubeapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class YouTubeVideoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
