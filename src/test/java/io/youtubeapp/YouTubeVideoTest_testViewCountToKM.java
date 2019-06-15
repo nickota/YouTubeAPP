7cd8de57b9912eb6928ee61a037048aeb94cd2d3
nickota <xxk0ta0605@gmail.com>
2019-06-15 11:47:57 +0900

    finish

--- src/test/java/io/youtubeapp/YouTubeVideoTest_testViewCountToKM.java
+++ src/test/java/io/youtubeapp/YouTubeVideoTest_testViewCountToKM.java
@@ -1,32 +1,22 @@
 package io.youtubeapp;
 
-import io.youtubeapp.model.YouTubeVideo;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import io.youtubeapp.model.YouTubeVideo;
 
 class YouTubeVideoTest_testViewCountToKM {
 
 	@ParameterizedTest
-	@CsvSource({
-		"null,'0'",
-		"-1, '0'",
-		"0, '0'",
-		"999,'999'",
-		"1000,'1K'",
-		"999999,'999K'",
-		"1000000,'1M'"
-	})
+	@CsvSource({ "null,'0'", "-1, '0'", "0, '0'", "999,'999'", "1000,'1K'", "999999,'999K'", "1000000,'1M'" })
 	void testViewCountToKM(BigInteger num, String str) {
 		YouTubeVideo video = new YouTubeVideo();
 		video.setViewCount(num);
-		
+
 		assertThat(video.viewCountToKM()).isEqualTo(str);
 	}
 }
